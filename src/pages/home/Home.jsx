035071648500b38dfc1c2d36f062d18701ba97fd
AzizAzizli <azizliaziz025@gmail.com>
2023-11-07 20:41:35 +0400

    update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,43 +1,32 @@
-import React, {  useRef, useState } from "react";
-import axios from "axios";
+import React, { useEffect, useRef, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { MovieCard } from "../../components/moviecard/MovieCard";
 import "./Home.css";
 
 export const Home = () => {
   const [movies, setMovies] = useState([]);
-  let searchInput = useRef();
+  const [loading, setLoading] = useState(false); // Add loading state
+  const searchInput = useRef();
 
-
- function handleInput() {
+  async function handleInput() {
+    setLoading(true); 
     let value = searchInput.current.value;
-    console.log(value);
-    const data = getMovies(value);
+    const data = await getMovies(value);
     setMovies(data);
     searchInput.current.value = "";
     searchInput.current.focus();
-    console.log(movies);
+    setLoading(false); 
   }
 
-  function getMovies(title) {
+  async function getMovies(title) {
     const url = `http://www.omdbapi.com/?apikey=a407a7b3&s=${title}`;
-
-    axios({ method: "GET", url }).then((res) => {
-     
-      const resData = res.data.Search;
-
-      console.log(resData);
-
-      setMovies(resData);
-    });
+    let res = await fetch(url);
+    let result = await res.json();
+    return result.Search;
   }
-function a(){
-  setTimeout(() =>{
-  return <h1 className=" fw-bold text-warning">Not found...</h1>
-  },500)
-}
+
   return (
-    <div className="  d-flex flex-column gap-5 align-items-center row pt-5">
+    <div className="d-flex flex-column gap-5 align-items-center row pt-5">
       <h1 className="fw-bold text-center text-primary">Movie app</h1>
       <div className="d-flex justify-content-center gap-3 col-xl-5 col-lg-7 col-sm-9 col-11">
         <input ref={searchInput} placeholder="Search" type="search" className="movieInput" />
@@ -46,9 +35,15 @@ function a(){
         </button>
       </div>
       <div className="d-flex flex-wrap gap-3 w-100 justify-content-center align-items-center container">
-        {movies?movies.map((movie) => (
-          <MovieCard key={movie.imdbID} data={movie}  movies={movies}/>
-        )): a()}
+        {loading ? (
+          <h1 className="fw-bold text-primary">Loading...</h1>
+        ) : movies ? (
+          movies.map((movie) => (
+            <MovieCard key={movie.imdbID} data={movie} movies={movies} />
+          ))
+        ) : (
+          <h1 className="fw-bold text-warning">Not found...</h1>
+        )}
       </div>
     </div>
   );
